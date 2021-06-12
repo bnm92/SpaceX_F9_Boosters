baf039859d70933589cb6dd28daa8735295a71c9
BrunoN <yayui67@gmail.com>
2021-06-12 05:00:42 +0200

    Adding BoosterRepository Interface.

--- src/main/java/com/example/demo/booster/BoosterRepository.java
+++ src/main/java/com/example/demo/booster/BoosterRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.booster;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface BoosterRepository extends JpaRepository<Booster, Long> {
-
+    @Query("SELECT b FROM Booster b WHERE b.serialNumber = ?1")
+    Optional<Booster> findStudentBySerialNumber(String serialNumber);
 }
